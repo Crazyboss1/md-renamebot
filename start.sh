2c8b8be11b6988f7debcfdaca9f9427c3c83eee8
Crazyboss1 <112222385+Crazyboss1@users.noreply.github.com>
2023-02-07 18:20:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repository"
-git clone https://github.com/Greymattersbot/md-renamebot /md-renamebot
-cd /md-renamebot 
+git clone https://github.com/CrazyBoss1/md-renamebot /app
+cd /app
 echo "installing requirements"
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
